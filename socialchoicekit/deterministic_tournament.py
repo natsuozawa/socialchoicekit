43c9b2be1767167b9720d2902c77857d75158465
Natsu Ozawa <natsuozawa@outlook.com>
2023-10-09 00:25:17 +0100

    Finish Copeland

---
+++ socialchoicekit/deterministic_tournament.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+class BaseTournament:
+  """
+  The abstract tournament rule. This class should not be instantiated directly.
+
+  Parameters
+  :type tie_breaker: str
+  Accepts the following
+  - "random": pick from a uniform distribution among the winners
+  - "accept": return all winners in an array
+
+  :type zero_indexed: bool
+  If True, the output of the social welfare function and social choice function will be zero-indexed. If False, the output will be one-indexed. One-indexed by default.
+  """
+  def __init__(
+    self,
+    tie_breaker: str="random",
+    zero_indexed: bool=False
+  ) -> None:
+    self.tie_breaker = tie_breaker
+    self.index_fixer = 0 if zero_indexed else 1
+    self._check_tie_breaker()
+
+  def swf(self, score: np.ndarray) -> np.ndarray:
+    rank = np.argsort(-score)
+    return np.array([rank + self.index_fixer, score[rank]])
+
+  def scf(self, score: np.ndarray) -> np.ndarray or int:
+    winners = np.argwhere(score == np.amax(score)).flatten() + self.index_fixer
+    if self.tie_breaker == "random":
+      return np.random.choice(winners)
+    return winners
+
+  def _check_profile(self, profile) -> None:
+    if isinstance(profile, np.ndarray):
+      if np.ndim(profile) == 2:
+        if np.amin(profile) == 1 and np.amax(profile) == profile.shape[1]:
+          return
+        raise ValueError("Profile must contain exactly integers from 1 to M")
+      raise ValueError("Profile must be a two-dimensional array")
+    # TODO: turn this into a common utils method and accept other formats
+    raise ValueError("Profile is not in a recognized data format")
+
+  def _check_tie_breaker(self) -> None:
+    if self.tie_breaker in ["random", "accept"]:
+      return
+    raise ValueError("Tie breaker is not recognized")
+
+class Copeland(BaseTournament):
+  """
+  Copeland rewards an alternative x for each pairwise victory x > y over an opponent and punishes her for each defeat, but disregards the margins of victory or defeat.
+  """
+
+  def score(self, profile: np.ndarray) -> np.ndarray:
+    """
+    The scoring function for this voting rule. Returns a list of alternatives with their scores.
+
+    Complexity
+    O(MN)
+
+    Parameters
+    :type profile: np.ndarray
+
+    A (N, M) array, where N is the number of voters and M is the number of alternatives. The element at (i, j) indicates the voter's preference for alternative j, where 1 is the most preferred alternative and M is the least preferred alternative.
+
+    Returns
+
+    :return np.ndarray
+    A (1, M) array of scores where the element at (0, j) indicates the score for alternative j.
+    """
+    self._check_profile(profile)
+    net_preferences = np.zeros((profile.shape[1], profile.shape[1]))
+    for i in range(profile.shape[1]):
+      preferences_by_voter = profile - profile[:, i].reshape(profile.shape[0], 1)
+      preferences_by_voter = np.where(preferences_by_voter > 0, 1, preferences_by_voter)
+      preferences_by_voter = np.where(preferences_by_voter < 0, -1, preferences_by_voter)
+      preferences_by_voter = np.sum(preferences_by_voter, axis=0)
+      net_preferences[i, :] = preferences_by_voter
+
+    score = np.where(net_preferences > 0, 1, net_preferences)
+    score = np.where(score < 0, -1, score)
+    score = np.sum(score, axis=1).T
+
+    return score
+
+  def swf(self, profile: np.ndarray) -> np.ndarray:
+    """
+    The social welfare function for this voting rule. Returns a ranked list of alternatives with the scores. Note that tie breaking behavior is undefined.
+
+    Complexity
+    O(MN + MlogM)
+
+    Parameters
+    :type profile: np.ndarray
+
+    A (N, M) array, where N is the number of voters and M is the number of alternatives. The element at (i, j) indicates the voter's preference for alternative j, where 1 is the most preferred alternative and M is the least preferred alternative.
+
+    Returns
+
+    :return np.ndarray
+    A (2, M) array of scores where the element at (0, j) indicates the alternative number for the jth alternative and (1, j) indictes the score for the jth alternative.
+    """
+    self._check_profile(profile)
+    score = self.score(profile)
+    return super().swf(score)
+
+  def scf(self, profile: np.ndarray) -> np.ndarray or int:
+    """
+    The social choice function for this voting rule. Returns a set of alternatives with the highest scores. With a tie breaking rule, returns a single alternative.
+
+    Complexity
+    O(MN)
+
+    Parameters
+    :type profile: np.ndarray
+
+    A (N, M) array, where N is the number of voters and M is the number of alternatives. The element at (i, j) indicates the voter's preference for alternative j, where 1 is the most preferred alternative and M is the least preferred alternative.
+
+    Returns
+    :return np.ndarray or int
+    """
+    self._check_profile(profile)
+    score = self.score(profile)
+    return super().scf(score)
