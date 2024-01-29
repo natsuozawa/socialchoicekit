09f3c7d5603a8c87d232f8f409dd4d5ba2aa65f5
Natsu Ozawa <natsuozawa@outlook.com>
2024-01-29 20:51:37 +0000

    Add hospital oriented Gale Shapley

--- socialchoicekit/deterministic_matching.py
+++ socialchoicekit/deterministic_matching.py
@@ -5,20 +5,24 @@ import heapq
 
 from socialchoicekit.utils import check_profile
 
-class ResidentOrientedGaleShapley:
+class GaleShapley:
   """
   Resident-oriented Gale Shapley algorithm (RGS) is a deferred acceptance algorithm that finds a stable matching in the two sided matching setting. It is resident optimal.
 
   Parameters
   ----------
+  resident_oriented : bool
+    If True, the social choice function will be resident-oriented. If False, the social choice function will be hospital-oriented. Resident-oriented by default.
   zero_indexed : bool
     If True, the output of the social choice function will be zero-indexed. If False, the output will be one-indexed. One-indexed by default.
   """
   def __init__(
     self,
+    resident_oriented: bool = True,
     zero_indexed: bool = False,
   ):
     self.index_fixer = 0 if zero_indexed else 1
+    self.resident_oriented = resident_oriented
 
   def scf(
     self,
@@ -54,64 +58,119 @@ class ResidentOrientedGaleShapley:
     resident_profile = resident_profile - 1
     hospital_profile = hospital_profile - 1
 
-    # Key: resident, value = the last hospital the resident applied to
-    resident_applications = {}
-    # Key: hospital, value = priority queue of residents the hospital is matched to,
-    # where each resident is expressed as the ranked position for that hospital.
-    hospital_waiting_lists = {i: [] for i in range(m)}
-
     # NaN will be put last.
     ranked_resident_profile = np.argsort(resident_profile, axis=1)
     ranked_hospital_profile = np.argsort(hospital_profile, axis=1)
 
-    # Initially, everyone applies.
-    next_current_applicants = np.ones(n, dtype=int)
+    if self.resident_oriented:
+      # Key: resident, value = the last hospital the resident applied to
+      resident_applications = {}
 
-    while True:
-      if np.all(next_current_applicants != 1):
-        break
+      # Key: hospital, value = list of residents the hospital is matched to,
+      # where each resident is expressed as the ranked position for that hospital.
+      # In resident-oriented Gale Shapley this is a priority queue.
+      hospital_waiting_lists = {i: [] for i in range(m)}
 
-      # Copy because we don't want the modification to take effect until the next iteration of the loop.
-      current_applicants = np.array(next_current_applicants)
+      # Initially, everyone applies.
+      next_current_applicants = np.ones(n, dtype=int)
 
-      # resident, next_hospital, dropped_resident are 0-indexed positions originally supplied in the input.
-      # last_applied_hospital_rank is a 0-indexed position in the ranked resident profile.
+      while True:
+        if np.all(next_current_applicants != 1):
+          break
+
+        # Copy because we don't want the modification to take effect until the next iteration of the loop.
+        current_applicants = np.array(next_current_applicants)
+
+        # resident, next_hospital, dropped_resident are 0-indexed positions originally supplied in the input.
+        # last_applied_hospital_rank is a 0-indexed position in the ranked resident profile.
+        for resident in range(n):
+          if current_applicants[resident] == 0 or current_applicants[resident] == 2:
+            # Resident already has a match or rejection is confirmed.
+            continue
+
+          last_applied_hospital_rank = resident_applications.get(resident, -1)
+          next_hospital = ranked_resident_profile[resident, last_applied_hospital_rank + 1]
+          if np.isnan(resident_profile[resident, next_hospital]):
+            # Candidate has applied to all hospitables they find acceptable. (Yet have not gotten accepted into any)
+            next_current_applicants[resident] = 2
+            continue
+
+          resident_applications[resident] = last_applied_hospital_rank + 1
+
+          if np.isnan(hospital_profile[next_hospital, resident]):
+            # Candidate is unacceptable to the hospital. Auto-rejected.
+            continue
+
+          hospital_waiting_list = hospital_waiting_lists.get(next_hospital, [])
+          # Negate resident rank because heapq is a min heap.
+          heapq.heappush(hospital_waiting_list, int(hospital_profile[next_hospital, resident] * -1))
+          next_current_applicants[resident] = 0
+
+          if len(hospital_waiting_list) <= c[next_hospital]:
+            # Hospital has not reached capacity yet.
+            continue
+
+          # Hospital has reached capacity.
+          # Revert back from negated resident rank
+          dropped_resident = ranked_hospital_profile[next_hospital, heapq.heappop(hospital_waiting_list) * -1]
+          next_current_applicants[dropped_resident] = 1
+
+      ans = []
+      for hospital in range(m):
+        for resident_rank in hospital_waiting_lists.get(hospital, []):
+          # Revert back from negated resident rank
+          ans.append((int(ranked_hospital_profile[hospital, resident_rank * -1]) + self.index_fixer, hospital + self.index_fixer))
+      return ans
+
+    else:
+      # Key: resident, value = the last resident the hospital offered to
+      hospital_offers = {}
+
+      resident_waiting_lists = {i: -1 for i in range(n)}
+
+      # np.nan if hospital is terminally undersubscribed.
+      hospital_accepted_offers = np.zeros(m, dtype=int)
+      current_offerers = np.ones(m, dtype=int)
+
+      while True:
+        current_offerers = np.where(current_offerers == 2, 2, np.where(c == hospital_accepted_offers, 0, 1))
+        if np.all(current_offerers != 1):
+          break
+
+        # hospital, next_resident, dropped_hospital are 0-indexed positions originally supplied in the input.
+        # last_applied_resident_rank is a 0-indexed position in the ranked resident profile.
+        for hospital in range(m):
+          if current_offerers[hospital] == 0 or current_offerers[hospital] == 2:
+            # Hospital already has a match or undersubscription is confirmed.
+            continue
+
+          last_applied_resident_rank = hospital_offers.get(hospital, -1)
+          next_resident = ranked_hospital_profile[hospital, last_applied_resident_rank + 1]
+          if np.isnan(hospital_profile[hospital, next_resident]):
+            # Hospital has offered to all residents they find acceptable. (Yet are undersubscribed)
+            current_offerers[hospital] = 2
+
+          hospital_offers[hospital] = last_applied_resident_rank + 1
+
+          if np.isnan(resident_profile[next_resident, hospital]):
+            # Hospital is unacceptable to the resident. Auto-rejected.
+            continue
+
+          # Negate resident rank because heapq is a min heap.
+          current_accepted_hospital = resident_waiting_lists[next_resident]
+          if current_accepted_hospital == -1 or resident_profile[next_resident, hospital] < resident_profile[next_resident, current_accepted_hospital]:
+            # Resident has not received any offers yet or the hospital is more preferred than the resident's current offer.
+            hospital_accepted_offers[hospital] += 1
+            hospital_accepted_offers[current_accepted_hospital] -= 1
+            resident_waiting_lists[next_resident] = hospital
+
+      ans = []
       for resident in range(n):
-        if current_applicants[resident] == 0 or current_applicants[resident] == 2:
-          # Resident already has a match or rejection is confirmed.
+        hospital = resident_waiting_lists.get(resident, -1)
+        if hospital == -1:
           continue
+        ans.append((resident + self.index_fixer, hospital + self.index_fixer))
+      return ans
 
-        last_applied_hospital_rank = resident_applications.get(resident, -1)
-        next_hospital = ranked_resident_profile[resident, last_applied_hospital_rank + 1]
-        if np.isnan(resident_profile[resident, next_hospital]):
-          # Candidate has applied to all hospitables they find acceptable. (Yet have not gotten accepted into any)
-          next_current_applicants[resident] = 2
-          continue
 
-        resident_applications[resident] = last_applied_hospital_rank + 1
-
-        if np.isnan(hospital_profile[next_hospital, resident]):
-          # Candidate is unacceptable to the hospital. Auto-rejected.
-          continue
-
-        hospital_waiting_list = hospital_waiting_lists.get(next_hospital, [])
-        # Negate resident rank because heapq is a min heap.
-        heapq.heappush(hospital_waiting_list, int(hospital_profile[next_hospital, resident] * -1))
-        next_current_applicants[resident] = 0
-
-        if len(hospital_waiting_list) <= c[next_hospital]:
-          # Hospital has not reached capacity yet.
-          continue
-
-        # Hospital has reached capacity.
-        # Revert back from negated resident rank
-        dropped_resident = ranked_hospital_profile[next_hospital, heapq.heappop(hospital_waiting_list) * -1]
-        next_current_applicants[dropped_resident] = 1
-
-    ans = []
-    for hospital in range(m):
-      for resident_rank in hospital_waiting_lists.get(hospital, []):
-        # Revert back from negated resident rank
-        ans.append((int(ranked_hospital_profile[hospital, resident_rank * -1]) + self.index_fixer, hospital + self.index_fixer))
-    return ans
 
--- tests/unit/test_deterministic_matching.py
+++ tests/unit/test_deterministic_matching.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import pytest
 
-from socialchoicekit.deterministic_matching import ResidentOrientedGaleShapley
+from socialchoicekit.deterministic_matching import GaleShapley
 
 class TestDeterministicMatching:
   # Example from Handbook of Computational Social Choice, Chapter 14.
@@ -28,10 +28,18 @@ class TestDeterministicMatching:
     return np.array([1, 2, 1])
 
   def test_basic_profile_1(self, basic_resident_profile_1, basic_hospital_profile_1, basic_c_1):
-    rogs = ResidentOrientedGaleShapley()
-    assignments = rogs.scf(basic_resident_profile_1, basic_hospital_profile_1, basic_c_1)
-    assert len(assignments) == 3
-    assert (1, 2) in assignments
-    assert (2, 1) in assignments
-    assert (3, 2) in assignments
+    rogs = GaleShapley(resident_oriented=True)
+    rogs_assignments = rogs.scf(basic_resident_profile_1, basic_hospital_profile_1, basic_c_1)
+    assert len(rogs_assignments) == 3
+    assert (1, 2) in rogs_assignments
+    assert (2, 1) in rogs_assignments
+    assert (3, 2) in rogs_assignments
+
+    hogs = GaleShapley(resident_oriented=False)
+    hogs_assignments = hogs.scf(basic_resident_profile_1, basic_hospital_profile_1, basic_c_1)
+    assert len(hogs_assignments) == 3
+    assert (3, 2) in hogs_assignments
+    assert (2, 1) in hogs_assignments
+    assert (1, 2) in hogs_assignments
+
 
