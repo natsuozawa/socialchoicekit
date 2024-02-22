99099a9b2c7e9ee64ad61cbea32c24fbae1586a5
Natsu Ozawa <natsuozawa@outlook.com>
2024-02-22 15:36:03 +0000

    Add capacity test for GS

--- tests/unit/test_deterministic_matching.py
+++ tests/unit/test_deterministic_matching.py
@@ -28,7 +28,7 @@ class TestDeterministicMatching:
   def basic_c_1(self):
     return np.array([1, 2, 1])
 
-  def test_basic_profile_1(self, basic_resident_profile_1, basic_hospital_profile_1, basic_c_1):
+  def test_gale_shapley_basic_profile_1(self, basic_resident_profile_1, basic_hospital_profile_1, basic_c_1):
     rogs = GaleShapley(resident_oriented=True)
     rogs_assignments = rogs.scf(basic_resident_profile_1, basic_hospital_profile_1, basic_c_1)
     assert len(rogs_assignments) == 3
@@ -43,4 +43,19 @@ class TestDeterministicMatching:
     assert (2, 1) in hogs_assignments
     assert (1, 2) in hogs_assignments
 
+  def test_gale_shapley_capacity_requirement(self, basic_resident_profile_1, basic_hospital_profile_1):
+    m = basic_hospital_profile_1.shape[1]
 
+    rogs = GaleShapley(resident_oriented=True)
+    rogs_assignments = rogs.scf(basic_resident_profile_1, basic_hospital_profile_1, np.ones(3))
+    rogs_hospital_assignment_counter = {hospital: 0 for hospital in range(1, m + 1)}
+    for h, _ in rogs_assignments:
+      rogs_hospital_assignment_counter[h] += 1
+      assert rogs_hospital_assignment_counter[h] == 1
+
+    hogs = GaleShapley(resident_oriented=False)
+    hogs_assignments = hogs.scf(basic_resident_profile_1, basic_hospital_profile_1, np.ones(3))
+    hogs_hospital_assignment_counter = {hospital: 0 for hospital in range(1, m + 1)}
+    for h, _ in hogs_assignments:
+      hogs_hospital_assignment_counter[h] += 1
+      assert hogs_hospital_assignment_counter[h] == 1
