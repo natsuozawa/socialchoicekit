38984356c6ea007bfad53c0bd18ca02c79d7359b
Natsu Ozawa <natsuozawa@outlook.com>
2024-04-19 20:41:16 +0100

    Update setup.py license and version

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup
 
 setup(
     name='socialchoicekit',
-    version='0.0.3',
+    version='1.0.0',
     description='socialchoicekit aims to be a comprehensive implementation of the most important rules in computational social choice theory.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Natsu Ozawa',
     author_email='natsuozawa@outlook.com',
     url='https://github.com/natsuozawa/socialchoicekit',
-    # TODO: set license
-    # license='xxx',
+    license='MIT',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        # TODO: Add license classifier
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
